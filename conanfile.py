36893f895f2e3afb44cfbf4657bce09c45b9c4c3
SSE4 <tomskside@gmail.com>
2017-12-12 19:12:00 +0700

    - initial prototype

--- conanfile.py
+++ conanfile.py
@@ -1,48 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
 import os
 
 
 class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    url = "https://github.com/bincrafters/conan-libname"
-    description = "Keep it short"
-    license = "https://github.com/someauthor/somelib/blob/master/LICENSES"
+    name = "libvpx"
+    version = "1.6.1"
+    url = "https://github.com/bincrafters/conan-libvpx"
+    description = "WebM VP8/VP9 Codec SDK"
+    license = "https://github.com/webmproject/libvpx/blob/master/LICENSE"
     exports_sources = ["CMakeLists.txt", "LICENSE"]
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    #use static org/channel for libs in conan-center
-    #use dynamic org/channel for libs in bincrafters
-    requires = "OpenSSL/1.0.2l@conan/stable", \
-        "zlib/1.2.11@conan/stable", \
-        "websocketpp/0.7.0@%s/%s" % (self.user, self.channel)
+
+    def build_requirements(self):
+        self.build_requires("yasm_installer/[>=1.3.0]@bincrafters/stable")
 
     def source(self):
-        source_url = "https://github.com/Microsoft/cpprestsdk"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        source_url = "https://github.com/webmproject/libvpx/archive/v%s.tar.gz" % self.version
+        tools.get(source_url)
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, "sources")
-        #Rename to "sources" is a convention to simplify later steps
 
     def build(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
-        cmake.configure(source_dir="sources")
-        cmake.build()
+        with tools.chdir('sources'):
+            args = ['--prefix=%s' % self.package_folder,
+                    '--disable-examples',
+                    '--disable-unit-tests',
+                    '--enable-vp9-highbitdepth',
+                    '--as=yasm']
+            if self.options.shared:
+                args.extend(['--disable-static', '--enable-shared'])
+            else:
+                args.extend(['--disable-shared', '--enable-static'])
+            env_build = AutoToolsBuildEnvironment(self)
+            env_build.configure(args=args)
+            env_build.make()
+            env_build.make(args=['install'])
 
     def package(self):
-        with tools.chdir("sources"):
-            self.copy(pattern="LICENSE")
-            self.copy(pattern="*", dst="include", src="include")
-            self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
-            self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="LICENSE", src='sources')
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,8 +1,9 @@
 #include <cstdlib>
 #include <iostream>
+#include "vpx/vpx_codec.h"
 
 int main()
 {
-    std::cout << "Bincrafters\n";
+    std::cout << "vpx version " << vpx_codec_version_str() << std::endl;
     return EXIT_SUCCESS;
 }
