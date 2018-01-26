c62ed75672ab4e36e665770cdf107e66375517c5
SSE4 <tomskside@gmail.com>
2018-01-26 13:28:34 +0700

    - Windows builds require cygwin

--- build.py
+++ build.py
@@ -2,10 +2,19 @@
 # -*- coding: utf-8 -*-
 
 
-from bincrafters import build_template_default
+from bincrafters import build_template_default, build_shared
+import platform
+import os
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder()
+    builder = build_template_default.get_builder(pure_c=False)
+
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        if build_shared.get_os() == "Windows":
+            installers = ["cygwin_installer/2.9.0@bincrafters/stable"]
+            if os.getenv('MINGW_CONFIGURATIONS', ''):
+                installers.append("mingw_installer/1.0@conan/stable")
+            build_requires.update({"*": installers})
 
     builder.run()
