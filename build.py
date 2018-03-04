d88daf2ed74a627ed5602fd6e8f4279210729de1
SSE4 <tomskside@gmail.com>
2018-03-04 16:16:29 +0700

    - yasm is always required

--- build.py
+++ build.py
@@ -11,10 +11,11 @@ if __name__ == "__main__":
     builder = build_template_default.get_builder(pure_c=True)
 
     for settings, options, env_vars, build_requires, reference in builder.items:
+        installers = ["yasm_installer/1.3.0@bincrafters/stable"]
         if build_shared.get_os() == "Windows":
-            installers = ["cygwin_installer/2.9.0@bincrafters/stable", "yasm_installer/1.3.0@bincrafters/stable"]
+            installers.append("cygwin_installer/2.9.0@bincrafters/stable")
             if os.getenv('MINGW_CONFIGURATIONS', ''):
                 installers.append("mingw_installer/1.0@conan/stable")
-            build_requires.update({"*": installers})
+        build_requires.update({"*": installers})
 
     builder.run()
