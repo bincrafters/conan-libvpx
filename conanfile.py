272d0ffeeeba3571b310a51576b110b092211e22
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-07-08 19:51:14 +0200

    Add msys2_installer requirement + ignore more args for vcxproj generator

--- conanfile.py
+++ conanfile.py
@@ -20,11 +20,8 @@ class LibVPXConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {'shared': False, 'fPIC': True}
 
-    build_requires = (
-        'yasm_installer/1.3.0@bincrafters/stable',
-    )
-
     _source_subfolder = "source_subfolder"
+
 
     def configure(self):
         del self.settings.compiler.libcxx
@@ -35,6 +32,11 @@ class LibVPXConan(ConanFile):
         if self.settings.os == 'Windows':
             del self.options.fPIC
 
+    def build_requirements(self):
+        self.build_requires('yasm_installer/1.3.0@bincrafters/stable')
+        if tools.os_info.is_windows:
+            self.build_requires('msys2_installer/20161025@bincrafters/stable')
+
     def source(self):
         source_url = "https://github.com/webmproject/libvpx/archive/v%s.tar.gz" % self.version
         tools.get(source_url, sha256="86df18c694e1c06cc8f83d2d816e9270747a0ce6abe316e93a4f4095689373f6")
@@ -42,20 +44,22 @@ class LibVPXConan(ConanFile):
         os.rename(extracted_dir, self._source_subfolder)
 
     def _fix_sources(self):
-        if self.settings.compiler == 'Visual Studio':
-            gen = os.path.join(self._source_subfolder, 'build', 'make', 'gen_msvs_vcxproj.sh')
-            tools.replace_in_file(gen,
-                                  '        --help|-h) show_help',
-                                  '        --help|-h) show_help\n        ;;\n        -O*) echo "ignoring -O..."\n')
-            tools.replace_in_file(gen,
-                                  '        --help|-h) show_help',
-                                  '        --help|-h) show_help\n        ;;\n        -Zi) echo "ignoring -Zi..."\n')
-            # disable warning:
-            # vpx.lib(vpx_src_vpx_image.obj) : MSIL .netmodule or module compiled with /GL found; restarting link
-            # with /LTCG; add /LTCG to the link command line to improve linker performance
-            tools.replace_in_file(gen,
-                                  'tag_content WholeProgramOptimization true',
-                                  'tag_content WholeProgramOptimization false')
+        gen = os.path.join(self._source_subfolder, 'build', 'make', 'gen_msvs_vcxproj.sh')
+        tools.replace_in_file(gen,
+                              '        --help|-h) show_help',
+                              '        --help|-h) show_help\n        ;;\n        -O*) echo "ignoring $opt..."\n')
+        tools.replace_in_file(gen,
+                              '        --help|-h) show_help',
+                              '        --help|-h) show_help\n        ;;\n        -Zi) echo "ignoring $opt..."\n')
+        tools.replace_in_file(gen,
+                              '        --help|-h) show_help',
+                              '        --help|-h) show_help\n        ;;\n        -MD*|-MT*) echo "ignoring $opt (this option is already handled)"\n')
+        # disable warning:
+        # vpx.lib(vpx_src_vpx_image.obj) : MSIL .netmodule or module compiled with /GL found; restarting link
+        # with /LTCG; add /LTCG to the link command line to improve linker performance
+        tools.replace_in_file(gen,
+                              'tag_content WholeProgramOptimization true',
+                              'tag_content WholeProgramOptimization false')
 
     def _configure_autotools(self):
         win_bash = tools.os_info.is_windows
@@ -65,6 +69,7 @@ class LibVPXConan(ConanFile):
         args = ['--prefix=%s' % prefix,
                 '--disable-examples',
                 '--disable-unit-tests',
+                # '--disable-tools',
                 '--enable-vp9-highbitdepth',
                 '--as=yasm']
         if self.options.shared:
@@ -105,26 +110,19 @@ class LibVPXConan(ConanFile):
         if self.settings.compiler == 'apple-clang':
             if float(str(self.settings.compiler.version)) < 8.0:
                 args.append('--disable-avx512')
-        env_build = AutoToolsBuildEnvironment(self, win_bash=win_bash)
-        env_build.configure(args=args, configure_dir=self._source_subfolder, host=False, build=False, target=False)
+        with tools.vcvars(self.settings):
+            env_build = AutoToolsBuildEnvironment(self, win_bash=win_bash)
+            env_build.configure(args=args, configure_dir=self._source_subfolder, host=False, build=False, target=False)
         return env_build
 
     def build(self):
-        if self.settings.os == 'Windows':
-            cygwin_bin = self.deps_env_info['cygwin_installer'].CYGWIN_BIN
-            with tools.environment_append({'PATH': [cygwin_bin],
-                                           'CONAN_BASH_PATH': os.path.join(cygwin_bin, 'bash.exe')}):
-                if self.settings.compiler == 'Visual Studio':
-                    with tools.vcvars(self.settings, filter_known_paths=False):
-                        env_build = self._configure_autotools()
-                else:
-                    env_build = self._configure_autotools()
-        else:
+        self._fix_sources()
+        with tools.vcvars(self.settings):
             env_build = self._configure_autotools()
-        env_build.make()
+            env_build.make()
 
     def package(self):
-        env_build = self._configure_autotools()
+        env_build = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
         env_build.install()
 
         self.copy(pattern="LICENSE", src='sources', dst='licenses')
