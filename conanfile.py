885723204b304b8ab86b1f0b778b14d51289dfdf
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-07-08 13:53:15 +0200

    Use source_subfolder

--- conanfile.py
+++ conanfile.py
@@ -20,6 +20,8 @@ class LibVPXConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {'shared': False, 'fPIC': True}
 
+    _source_subfolder = "source_subfolder"
+
     def configure(self):
         del self.settings.compiler.libcxx
         if self.settings.os == 'Windows' and self.options.shared:
@@ -33,7 +35,7 @@ class LibVPXConan(ConanFile):
         source_url = "https://github.com/webmproject/libvpx/archive/v%s.tar.gz" % self.version
         tools.get(source_url, sha256="86df18c694e1c06cc8f83d2d816e9270747a0ce6abe316e93a4f4095689373f6")
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, "sources")
+        os.rename(extracted_dir, self._source_subfolder)
 
     def build(self):
         if self.settings.os == 'Windows':
@@ -49,72 +51,71 @@ class LibVPXConan(ConanFile):
             self.build_configure()
 
     def build_configure(self):
-        with tools.chdir('sources'):
-            if self.settings.compiler == 'Visual Studio':
-                gen = os.path.join('build', 'make', 'gen_msvs_vcxproj.sh')
-                tools.replace_in_file(gen,
-                                      '        --help|-h) show_help',
-                                      '        --help|-h) show_help\n        ;;\n        -O*) echo "ignoring -O..."\n')
-                tools.replace_in_file(gen,
-                                      '        --help|-h) show_help',
-                                      '        --help|-h) show_help\n        ;;\n        -Zi) echo "ignoring -Zi..."\n')
-                # disable warning:
-                # vpx.lib(vpx_src_vpx_image.obj) : MSIL .netmodule or module compiled with /GL found; restarting link
-                # with /LTCG; add /LTCG to the link command line to improve linker performance
-                tools.replace_in_file(gen,
-                                      'tag_content WholeProgramOptimization true',
-                                      'tag_content WholeProgramOptimization false')
-            win_bash = self.settings.os == 'Windows'
-            prefix = os.path.abspath(self.package_folder)
-            if self.settings.os == 'Windows':
-                prefix = tools.unix_path(prefix, tools.CYGWIN)
-            args = ['--prefix=%s' % prefix,
-                    '--disable-examples',
-                    '--disable-unit-tests',
-                    '--enable-vp9-highbitdepth',
-                    '--as=yasm']
-            if self.options.shared:
-                args.extend(['--disable-static', '--enable-shared'])
-            else:
-                args.extend(['--disable-shared', '--enable-static'])
-            if self.settings.os != 'Windows' and self.options.fPIC:
-                args.append('--enable-pic')
-            if self.settings.build_type == "Debug":
-                args.append('--enable-debug')
-            if self.settings.compiler == 'Visual Studio':
-                if 'MT' in str(self.settings.compiler.runtime):
-                    args.append('--enable-static-msvcrt')
+        if self.settings.compiler == 'Visual Studio':
+            gen = os.path.join(self._source_subfolder, 'build', 'make', 'gen_msvs_vcxproj.sh')
+            tools.replace_in_file(gen,
+                                  '        --help|-h) show_help',
+                                  '        --help|-h) show_help\n        ;;\n        -O*) echo "ignoring -O..."\n')
+            tools.replace_in_file(gen,
+                                  '        --help|-h) show_help',
+                                  '        --help|-h) show_help\n        ;;\n        -Zi) echo "ignoring -Zi..."\n')
+            # disable warning:
+            # vpx.lib(vpx_src_vpx_image.obj) : MSIL .netmodule or module compiled with /GL found; restarting link
+            # with /LTCG; add /LTCG to the link command line to improve linker performance
+            tools.replace_in_file(gen,
+                                  'tag_content WholeProgramOptimization true',
+                                  'tag_content WholeProgramOptimization false')
+        win_bash = self.settings.os == 'Windows'
+        prefix = os.path.abspath(self.package_folder)
+        if self.settings.os == 'Windows':
+            prefix = tools.unix_path(prefix, tools.CYGWIN)
+        args = ['--prefix=%s' % prefix,
+                '--disable-examples',
+                '--disable-unit-tests',
+                '--enable-vp9-highbitdepth',
+                '--as=yasm']
+        if self.options.shared:
+            args.extend(['--disable-static', '--enable-shared'])
+        else:
+            args.extend(['--disable-shared', '--enable-static'])
+        if self.settings.os != 'Windows' and self.options.fPIC:
+            args.append('--enable-pic')
+        if self.settings.build_type == "Debug":
+            args.append('--enable-debug')
+        if self.settings.compiler == 'Visual Studio':
+            if 'MT' in str(self.settings.compiler.runtime):
+                args.append('--enable-static-msvcrt')
 
-            arch = {'x86': 'x86',
-                    'x86_64': 'x86_64',
-                    'armv7': 'armv7',
-                    'armv8': 'arm64',
-                    'mips': 'mips32',
-                    'mips64': 'mips64',
-                    'sparc': 'sparc'}.get(str(self.settings.arch))
-            if self.settings.compiler == 'Visual Studio':
-                compiler = 'vs' + str(self.settings.compiler.version)
-            else:
-                compiler = 'gcc'
-            if self.settings.os == 'Windows':
-                os_name = 'win32' if self.settings.arch == 'x86' else 'win64'
-            elif str(self.settings.os) in ['Macos', 'iOS', 'watchOS', 'tvOS']:
-                os_name = 'darwin9'
-            elif self.settings.os == 'Linux':
-                os_name = 'linux'
-            elif self.settings.os == 'Solaris':
-                os_name = 'solaris'
-            elif self.settings.os == 'Android':
-                os_name = 'android'
-            target = "%s-%s-%s" % (arch, os_name, compiler)
-            args.append('--target=%s' % target)
-            if self.settings.compiler == 'apple-clang':
-                if float(str(self.settings.compiler.version)) < 8.0:
-                    args.append('--disable-avx512')
-            env_build = AutoToolsBuildEnvironment(self, win_bash=win_bash)
-            env_build.configure(args=args, host=False, build=False, target=False)
-            env_build.make()
-            env_build.make(args=['install'])
+        arch = {'x86': 'x86',
+                'x86_64': 'x86_64',
+                'armv7': 'armv7',
+                'armv8': 'arm64',
+                'mips': 'mips32',
+                'mips64': 'mips64',
+                'sparc': 'sparc'}.get(str(self.settings.arch))
+        if self.settings.compiler == 'Visual Studio':
+            compiler = 'vs' + str(self.settings.compiler.version)
+        else:
+            compiler = 'gcc'
+        if self.settings.os == 'Windows':
+            os_name = 'win32' if self.settings.arch == 'x86' else 'win64'
+        elif str(self.settings.os) in ['Macos', 'iOS', 'watchOS', 'tvOS']:
+            os_name = 'darwin9'
+        elif self.settings.os == 'Linux':
+            os_name = 'linux'
+        elif self.settings.os == 'Solaris':
+            os_name = 'solaris'
+        elif self.settings.os == 'Android':
+            os_name = 'android'
+        target = "%s-%s-%s" % (arch, os_name, compiler)
+        args.append('--target=%s' % target)
+        if self.settings.compiler == 'apple-clang':
+            if float(str(self.settings.compiler.version)) < 8.0:
+                args.append('--disable-avx512')
+        env_build = AutoToolsBuildEnvironment(self, win_bash=win_bash)
+        env_build.configure(args=args, configure_dir=self._source_subfolder, host=False, build=False, target=False)
+        env_build.make()
+        env_build.make(args=['install'])
 
     def package(self):
         self.copy(pattern="LICENSE", src='sources', dst='licenses')
